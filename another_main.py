ed8c6439b26b747dc10471ee3f6ac0663a137325
Youssef Nawar <youssef.nawar10@yahoo.com>
2021-12-02 01:28:29 +0200

    file explorer

--- another_main.py
+++ another_main.py
@@ -1,7 +1,12 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+
+import solutions
 
 # Set the app size
 # Window.size = (500, 700)
@@ -10,8 +15,27 @@ from kivy.uix.boxlayout import BoxLayout
 Builder.load_file('calc.kv')
 
 
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+    def show_load_list(self):
+        content = LoadDialog(load=self.load_list, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load a file list", content=content, size_hint=(0.5, 0.5))
+        self._popup.open()
+
+    def load_list(self, path, filename):
+        pass
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+    pass
+
+
 class CustomDropDown(BoxLayout):
     state = False
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
     def selection(self, text):
         if text == 'Bisection':
@@ -41,12 +65,14 @@ class CustomDropDown(BoxLayout):
             self.ids.gx.opacity = 0
         self.ids.dropdown.select(text)
 
-    def evaluate(self, inputText, selection):
+    def evaluate(self, inputText, selection, x1, x2, gx, maxIteration, precision):
         print(inputText, selection)
+        solutions.bisection()
         self.ids.answerField.text = inputText + " " + selection
 
     def upload(self):
-        pass
+        show = LoadDialog
+        LoadDialog.show_load_list(show)
 
 
 class CalculatorApp(App):
